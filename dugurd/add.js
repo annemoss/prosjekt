90d38aee74481044a6a492a7f64768e96e560fa5
annemoss <anne.mosvold@oerke.net>
2018-10-10 11:02:29 +0200

    add

---
+++ dugurd/add.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(e){
+	const header = document.querySelector("header")
+	header.innerHTML = "html"
+	const footer = document.querySelector("footer")
+    footer.innerHTML = "html"
+})
