5d9a322c309f7788b0fae97faaf82eb6741d38b5
Emilie Kalleberg <emilie.kalleberg@gmail.com>
2018-10-18 12:57:23 +0200

    aloha

---
+++ dugurd/scripts/knapp.js
@@ -0,0 +1,14 @@
+/* SØKNAD */
+function orderSuccseed {
+  var site = document.querySelector('#soknad');
+  var button = document.querySelector('#submit');
+  if button.clicked {
+    site => ....
+  }
+}
+
+/* BESTILL BORD*/
+var btn = document.querySelector('.button')
+btn.addEventListener('click', function(event) {
+  event.preventDefault();
+  btn.style.backgroundColor = 'pink';
