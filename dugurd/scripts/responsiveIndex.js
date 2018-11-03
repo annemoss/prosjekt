0f744b8c22a30ff632b478b300423493326410c7
sarunasj02 <sarunas02@gmail.com>
2018-11-03 19:57:03 +0100

    Desperate attempt at responsiv index

---
+++ dugurd/scripts/responsiveIndex.js
@@ -0,0 +1,26 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsive() {
+    var y = document.querySelector('.dropdownIndex');
+    y.addEventListener('click', function(){
+        changeDisp();
+    })
+
+    var x = document.querySelector(".navIndex");
+    if (x.className === "navIndex") {
+        x.className += " responsive";
+    } else {
+        x.className = "navIndex";
+    }
+}
+
+function changeDisp(){
+    var z = document.querySelector('.dropdown-contentIndex');
+
+    if(z.style.display == "none"){
+        z.style.display = 'block'
+    }
+    else{
+        z.style.display = 'none';
+    }
+    
+}
