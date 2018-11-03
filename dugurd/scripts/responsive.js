f8558b7846ccb08d47f0099dbe579962b3cedaa6
sarunasj02 <sarunas02@gmail.com>
2018-11-03 18:36:08 +0100

    Responsive navbar

--- dugurd/scripts/responsive.js
+++ dugurd/scripts/responsive.js
@@ -1,9 +1,26 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsive() {
+    var y = document.querySelector('.dropdown');
+    y.addEventListener('click', function(){
+        changeDisp();
+    })
+
     var x = document.querySelector(".nav");
     if (x.className === "nav") {
         x.className += " responsive";
     } else {
         x.className = "nav";
     }
+}
+
+function changeDisp(){
+    var z = document.querySelector('.dropdown-content');
+
+    if(z.style.display == "none"){
+        z.style.display = 'block'
+    }
+    else{
+        z.style.display = 'none';
+    }
+    
 }
