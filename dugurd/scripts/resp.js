a0d0852a13d6cd30d52dfc1240c5020c8375f565
gunvorhuso <gunvor_huso@hotmail.com>
2018-11-05 14:07:19 +0100

    n

--- dugurd/scripts/resp.js
+++
@@ -1,8 +0,0 @@
-function responsiveIndex() {
-    var x = document.querySelector(".navIndex");
-    if (x.className === "navIndex") {
-        x.className += " responsive";
-    } else {
-        x.className = "navIndex";
-    }
-}
