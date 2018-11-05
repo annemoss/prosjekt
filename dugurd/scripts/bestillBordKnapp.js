ad8a6a78cb2e665ff678a33df6e3006d89f0fa4e
Emilie Kalleberg <emilie.kalleberg@gmail.com>
2018-11-05 12:13:11 +0100

    div endringer

--- dugurd/scripts/bestillBordKnapp.js
+++ dugurd/scripts/bestillBordKnapp.js
@@ -1,4 +1,6 @@
 /* BESTILL BORD*/
+// QUESTION: ENDRE DATOFORM
+// QUESTION: SETTE KLOKKESLETT
 
 
 /*dato*/
@@ -31,20 +33,15 @@ else {
   time.setAttribute('max', '19:00:00');
 }
 
-/*span verdier*/
-const spanAntall = document.querySelector('.bordAntallPersoner');
-const spanDato = document.querySelector('.reservertDato');
-const spanTid = document.querySelector('.reservertKlokkeslett');
-const spanMail = document.querySelector('.mailBekreftelse');
 
 /*funksjonene for knapp*/
 const inputAntall = document.querySelector('#antall_manuell');
-const inputDato = 'HEI DATO'//document.querySelector('#dato');
-const inputKlokkeslett = 'HEI KLOKKESLETT'//document.querySelector('#klokkeslett');
-const inputEpost = document.querySelector('#epost');
+const inputDato = document.querySelector('#dato');
+//const inputDatoVerdi = moment(inputDato.value).format('DD/MM/YYYY');
+const inputKlokkeslett = document.querySelector('#klokkeslett');
+const inputMail = document.querySelector('#epost');
 const inputFornavn = document.querySelector('#fornavn');
 const inputEtternavn = document.querySelector('#etternavn');
-//???? const inputKjonn = document.querySelector('#kjonn');
 const inputMobil = document.querySelector('#telefonnummer');
 
 const reservasjon = document.querySelector('#reservasjonen');
@@ -54,103 +51,66 @@ const ikkeLedig = document.querySelector('#ikkeLedigReservasjon');
 const reservasjonFullfort = document.querySelector('#reservasjonFullfort');
 const leggInnKontaktInfo = document.querySelector('#leggInnKontaktInfo');
 
+/*span verdier*/
+const spanAntall = document.querySelector('#bordAntallPersoner');
+const spanAntall2 = document.querySelector('#bordAntallPersoner2');
+const spanAntall3 = document.querySelector('#bordAntallPersoner3');
+const spanDato = document.querySelector('#reservertDato');
+const spanDato2 = document.querySelector('#reservertDato2');
+const spanDato3 = document.querySelector('#reservertDato3');
+const spanTid = document.querySelector('#reservertKlokkeslett');
+const spanTid2 = document.querySelector('#reservertKlokkeslett2');
+const spanTid3 = document.querySelector('#reservertKlokkeslett3');
+const spanMail = document.querySelector('#mailBekreftelse');
+
+
+/*STARTOPPSETT*/
 ikkeLedig.style.display = 'none';
 reservasjonFullfort.style.display = 'none';
 leggInnKontaktInfo.style.display = 'none';
 
-
-// QUESTION: HVORDAN APPENDE VERDI TIL CLASS?
-// QUESTION: HVORDAN HENTE UT DATO VERDI OG TID VERDI???
 let tillfeldigTall = Math.random()*10
-btn.onclick = function LedigBord(event) {
-  inputAntallVerdi = inputAntall.value;
-  inputDatoVerdi = inputDato.value;
-  inputKlokkeslettVerdi = inputKlokkeslett.value;
-  /*APPENDER INPUTVERDEÌER TIL CLASS*/
-  spanAntall.append(inputAntallVerdi);
-  spanDato.append(inputDatoVerdi);
-  spanTid.append(inputKlokkeslettVerdi);
 
-  if ((inputAntall.checkValidity()) /*&& (inputDato.value.length !== 0) //SJEKKER IKKE DATO OG KLOKKESLETT
-   && (inputKlokkeslett.value.length !== 0)*/){
-      console.log("hei");
-      if (tillfeldigTall > 0){
+
+btn.onclick = function LedigBord(event) {
+
+if ((inputAntall.checkValidity()) && (inputDato.value !== '') //SJEKKER IKKE DATO OG KLOKKESLETT
+   && (inputKlokkeslett.value !== '')){
+// QUESTION: KLOKKESLETT ????
+
+      if (tillfeldigTall < 8){
         event.preventDefault()
         reservasjon.style.display = 'none';
         leggInnKontaktInfo.style.display = 'block';
+        leggInnKontaktInfo.scrollTo(0,0);
+        spanAntall.append(inputAntall.value);
+        spanDato.innerHTML = inputDato.value;
+        spanTid.append(inputKlokkeslett.value);
       }
         else {
           event.preventDefault()
           reservasjon.style.display = 'none';
           ikkeLedig.style.display = 'block';
+          spanAntall2.append(inputAntall.value);
+          spanDato2.innerHTML = inputDato.value;
+          spanTid2.append(inputKlokkeslett.value);
+
       }
-
   }
-
 }
 
 btn2.onclick = function KomplettReservasjon(event) {
-  //if ((inputFornavn.value!== '') && (inputEtternavn.value !== '')
-  //    /*&& (inputMobil.value !== '')*/&& (inputEpost.value !=='')){
+  if ((inputFornavn.value!== '') && (inputEtternavn.value !== '')
+  && (inputMail.value !=='')){
+
+    /*APPENDER INPUTVERDI TIL CLASS*/
+        spanAntall3.append(inputAntall.value);
+        spanDato3.innerHTML = inputDato.value;
+        spanTid3.append(inputKlokkeslett.value);
+        spanMail.append(inputMail.value);
+
     event.preventDefault()
     leggInnKontaktInfo.style.display='none';
     reservasjonFullfort.style.display='block';
-
-/*APPENDER INPUTVERDI TIL CLASS*/
-    spanMail.append(inputEpost.value);
-
-
-//  }
-
-}
-
-
-  /*spanAntall.append(inputAntall.value);
-  spanDato.append(inputDato.value);
-  spanTid.append(inputKlokkeslett.value);
-  spanMail.append(inputMail.value);*/
-/*
-
-var reservasjonen = document.getElementById('reservasjonen');
-var velgDato = document.getElementById('velgDato');
-var leggInnKontaktInfo = document.querySelector('#leggInnKontaktInfo');
-var ikkeLedig = document.getElementById('4');
-var reservasjonKomplett = document.getElementsByClassName('5');
-var randomtall = Math.random*10
-
-//p2.style.visibility = 'hidden';
-velgDato.style.display = 'none';
-velgDato.style.visibility = 'hidden';
-leggInnKontaktInfo.style.display = 'none';
-
-if ((valgAntallPersoner.display='block')){
-btn.onclick = function changeDisplay1 () {
-  p1.style.visibility = 'hidden';
-  p1.style.display = 'none';
-  p2.style.visibility = 'visible';
-  p2.style.display = 'block';
-  valg.style.visibility = 'hidden';
-  valg.style.display = 'none';
-  leggInnKontaktInfo.style.visibility = 'visible';
-  leggInnKontaktInfo.style.display = 'block';
   }
 }
-
-else if ((p2.visibility=='visible') {
-  btn.onclick = function changeDisplay2 (){
-    p2.style.visibility = 'hidden';
-    p2.style.display = 'none';
-    if (randomtall<8){
-      p3.style.visibility = 'visible';
-      p3.style.display = 'block';
-    }
-    else{
-      p4.style.visibility = 'visible';
-      p4.style.display = 'block';
-    }
-  }
-}
-else if ((p3.visibility=='visible')||(p4.visibility= ''){
-
-}
-*/
