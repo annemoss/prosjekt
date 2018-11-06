b194b45cd22ea9700c0df7cc447be0e32da8df46
annemoss <anne.mosvold@oerke.net>
2018-11-06 15:49:01 +0100

    sad

--- dugurd/scripts/responsiveIndex.js
+++ dugurd/scripts/responsiveIndex.js
@@ -1,26 +1,32 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsive() {
-    var y = document.querySelector('.dropdownIndex');
-    y.addEventListener('click', function(){
-        changeDisp();
-    })
+  var y = document.querySelector(".dropdownIndex");
+  y.addEventListener("click", function() {
+    changeDisp();
+  });
 
-    var x = document.querySelector(".navIndex");
-    if (x.className === "navIndex") {
-        x.className += " responsive";
-    } else {
-        x.className = "navIndex";
-    }
+  var x = document.querySelector(".navIndex");
+  if (x.className === "navIndex") {
+    x.className += " responsive";
+  } else {
+    x.className = "navIndex";
+  }
 }
 
-function changeDisp(){
-    var z = document.querySelector('.dropdown-contentIndex');
+function changeDisp() {
+  var z = document.querySelector(".dropdown-contentIndex");
 
-    if(z.style.display == "none"){
-        z.style.display = 'block'
-    }
-    else{
-        z.style.display = 'none';
-    }
+  if (z.style.display == "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+}
 
+var bilde = document.getElementById("minilogoIndex");
+
+function flytt_bilde() {
+  bilde.style.width = "80px";
+  bilde.style.height = "80px";
+  bilde.style.left = "0";
 }
