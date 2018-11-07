993c340c12ab95c06b1cbeb98517aed6a0dc7de9
sarunasj02 <sarunas02@gmail.com>
2018-11-07 10:22:52 +0100

    asd

--- dugurd/scripts/responsiveIndex.js
+++ dugurd/scripts/responsiveIndex.js
@@ -1,26 +1,34 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function responsive() {
-    var y = document.querySelector('.dropdownIndex');
-    y.addEventListener('click', function(){
-        changeDisp();
-    })
 
-    var x = document.querySelector(".navIndex");
-    if (x.className === "navIndex") {
-        x.className += " responsive";
-    } else {
-        x.className = "navIndex";
-    }
+var minilogiIndex = document.getElementById("minilogoIndex");
+
+function responsive() {
+  minilogiIndex.style.width = "80px";
+  minilogiIndex.style.height = "80px";
+  minilogiIndex.style.left = "10px";
+
+  var y = document.querySelector(".dropdownIndex");
+  y.addEventListener("click", function() {
+    changeDisp();
+  });
+
+  var x = document.querySelector(".navIndex");
+  if (x.className === "navIndex") {
+    x.className += " responsive";
+  } else {
+    x.className = "navIndex";
+    minilogiIndex.style.width = "155px";
+    minilogiIndex.style.height = "155px";
+    minilogiIndex.style.left = "35%";
+  }
 }
 
-function changeDisp(){
-    var z = document.querySelector('.dropdown-contentIndex');
+function changeDisp() {
+  var z = document.querySelector(".dropdown-contentIndex");
 
-    if(z.style.display == "none"){
-        z.style.display = 'block'
-    }
-    else{
-        z.style.display = 'none';
-    }
-
+  if (z.style.display == "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
 }
